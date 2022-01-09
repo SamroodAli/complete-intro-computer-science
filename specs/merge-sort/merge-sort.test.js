0b2e5ea5fb862c4376bd4329e9a096833773c5e9
Samrood Ali <devsamrood@gmail.com>
2022-01-09 11:43:43 +0530

    sort: implement merge sort solution

--- specs/merge-sort/merge-sort.test.js
+++ specs/merge-sort/merge-sort.test.js
@@ -6,13 +6,43 @@
   You'll need to write more than just one function
 */
 
+const merge = (lhs, rhs) => {
+  const result = [];
+
+  while (lhs.length && rhs.length) {
+    if (lhs[0] > rhs[0]) {
+      result.push(rhs.shift());
+    } else {
+      result.push(lhs.shift());
+    }
+  }
+
+  if (lhs.length) {
+    return result.concat(lhs);
+  } else if (rhs.length) {
+    return result.concat(rhs);
+  }
+  return result;
+};
+
 const mergeSort = (nums) => {
   // code goes here
+  if (!Array.isArray(nums)) {
+    throw new Error("merge Sort expects an array", `argument: ${nums}`);
+  }
+  if (nums.length <= 1) {
+    return nums;
+  }
+
+  const lhs = nums.slice(0, nums.length / 2);
+  const rhs = nums.slice(nums.length / 2, nums.length);
+
+  return merge(mergeSort(lhs), mergeSort(rhs));
 };
 
 // unit tests
 // do not modify the below code
-test.skip("merge sort", function () {
+test("merge sort", function () {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
   const ans = mergeSort(nums);
   expect(ans).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
