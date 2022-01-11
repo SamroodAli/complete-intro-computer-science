00d1ba38b7b4a452235ccaa8bf8ac7de701a0146
Samrood Ali <devsamrood@gmail.com>
2022-01-12 01:02:46 +0530

    search: implement binary search with pointers

---
+++ specs/search/search-pointers.test.js
@@ -0,0 +1,85 @@
+// for both exercises, the id of the object you're searching for is given to you
+// as integer. return the whole object that you're looking for
+//
+// it's up to you what to return if the object isn't found (we're not testing that)
+
+function linearSearch(id, array) {
+  // code goes here
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].id === id) {
+      return array[i];
+    }
+  }
+  return null;
+}
+
+function binarySearch(id, array) {
+  // code goes here
+  let arr = Array.from(array);
+  let left = 0;
+  let right = array.length;
+  while (left !== right) {
+    const middleIndex = Math.floor((left + right) / 2);
+    const middle = arr[middleIndex];
+    // console.(arr.length,arr)
+    // console.log(Math.floor(arr.length/2),arr)
+
+    if (middle.id === id) {
+      return middle;
+    }
+
+    if (middle.id > id) {
+      right = middleIndex;
+    }
+
+    if (middle.id < id) {
+      left = middleIndex;
+    }
+  }
+}
+
+// unit tests
+// do not modify the below code
+test("linear search", function () {
+  const lookingFor = { id: 5, name: "Brian" };
+  expect(
+    linearSearch(5, [
+      { id: 1, name: "Sam" },
+      { id: 11, name: "Sarah" },
+      { id: 21, name: "John" },
+      { id: 10, name: "Burke" },
+      { id: 13, name: "Simona" },
+      { id: 31, name: "Asim" },
+      { id: 6, name: "Niki" },
+      { id: 19, name: "Aysegul" },
+      { id: 25, name: "Kyle" },
+      { id: 18, name: "Jem" },
+      { id: 2, name: "Marc" },
+      { id: 51, name: "Chris" },
+      lookingFor,
+      { id: 14, name: "Ben" }
+    ])
+  ).toBe(lookingFor);
+});
+
+test("binary search", function () {
+  const lookingFor = { id: 23, name: "Brian" };
+  expect(
+    binarySearch(23, [
+      { id: 1, name: "Sam" },
+      { id: 3, name: "Sarah" },
+      { id: 5, name: "John" },
+      { id: 6, name: "Burke" },
+      { id: 10, name: "Simona" },
+      { id: 12, name: "Asim" },
+      { id: 13, name: "Niki" },
+      { id: 15, name: "Aysegul" },
+      { id: 17, name: "Kyle" },
+      { id: 18, name: "Jem" },
+      { id: 19, name: "Marc" },
+      { id: 21, name: "Chris" },
+      lookingFor,
+      { id: 24, name: "Ben" }
+    ])
+  ).toBe(lookingFor);
+});
